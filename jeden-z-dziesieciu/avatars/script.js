46658be7924309af331dbab4457a77abfa6c5923
Roxiqq <olszewskaroksana87@gmail.com>
2025-07-09 14:37:29 +0200

    Update script.js

--- jeden-z-dziesieciu/avatars/script.js
+++ jeden-z-dziesieciu/avatars/script.js
@@ -1,1 +1,22 @@
+let punkty = 0;
+let zycia = 3;
+
+function startGame() {
+  document.getElementById("pytanie").innerText = "Czy Ziemia krąży wokół Słońca?";
+}
+
+function sprawdzOdpowiedz(dobra) {
+  if (dobra) {
+    punkty += 1;
+    document.getElementById("punkty").innerText = "Punkty: " + punkty;
+  } else {
+    zycia -= 1;
+    const serca = "❤️".repeat(zycia);
+    document.getElementById("zycia").innerText = "Życia: " + serca;
+
+    if (zycia <= 0) {
+      document.getElementById("pytanie").innerText = "Koniec gry!";
+    }
+  }
+}
 
